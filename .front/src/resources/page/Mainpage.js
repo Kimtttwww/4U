8f4bb9664ab75d5c8ff704111a21969881510144
DESKTOP-SQKKJBG\user1 <taewan290@google.com>
2024-02-14 11:09:29 +0900

    통합본 ver 0.3

---
+++ .front/src/resources/page/Mainpage.js
@@ -0,0 +1,197 @@
+import React, {Component, useState} from 'react'
+import {Pagination, Navigation, Autoplay} from 'swiper/modules';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "../css/Mainpage.css";
+// import Button from 'react-bootstrap/Button';
+// import Modal from 'react-bootstrap/Modal';
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+
+export default () => {
+
+  return (
+    <>
+
+
+
+
+    <div>
+    <Swiper
+    modules={[Pagination, Navigation, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true}}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+      autoplay={{delay : 3000, disableOnInteraction: false}}
+    >
+      <SwiperSlide className='custom-slide'><img className='bannerImg' src='https://atimg.sonyunara.com/files/attrangs/goods/55939/63e9a881af788.jpg'/></SwiperSlide>
+      <SwiperSlide className='custom-slide'><img className='bannerImg' src='https://atimg.sonyunara.com/files/attrangs/goods/134915/64e30982eded0.jpg'/></SwiperSlide>
+      <SwiperSlide className='custom-slide'><img className='bannerImg' src='https://atimg.sonyunara.com/files/attrangs/goods/55939/1677981492_0.gif'/></SwiperSlide>
+      <SwiperSlide className='custom-slide'><img className='bannerImg' src='https://atimg.sonyunara.com/files/attrangs/goods/134915/1699945833_0.gif'/></SwiperSlide>
+      
+    </Swiper>
+    </div>
+    
+    {/*==============================================================================================================================================*/}
+
+
+    <div className='adTop'>
+     <div className='adTop-left'>
+          <img className = 'adTopImg' src='https://atimg.sonyunara.com/files/attrangs/goods/157385/65979a5371357.jpg'/>
+     </div>
+     <div className='adTop-right'>
+          2024 Spring 추천@ <br/> 데이트룩으로 좋아요~
+     </div>
+    </div>
+
+
+    <div className='adTop'>
+      <div className='adTop-leftTwo'>
+      데일리룩 찾으세요? <br/>
+      2024 Spring 베스트 추천해요
+      </div>
+      <div className='adTop-rightTwo'>
+      <img className = 'adTopImg' src='https://atimg.sonyunara.com/files/attrangs/goods/158300/65bc5879cc42c.jpg'/>
+      </div>
+    </div>
+    
+  {/*==============================================================================================================================================*/}
+
+  <h1>베스트 상품</h1>
+    <div className='clothListBox'>
+
+      <div className='clothList'>
+          <img className='listImg' src='https://atimg.sonyunara.com/files/attrangs/goods/36694/632ad827b1ce0.jpg' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+      
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+      
+      
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+      
+
+      
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+      
+      
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+      
+      
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+      
+    </div>
+
+    <h1>아우터</h1>
+    <div className='clothListBox'>
+
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+       <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+
+      </div>
+      <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+       <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+       <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+       <div className='clothList'>
+          <img src='...' alt='제품 이미지' />
+          <div className='clothContent'>
+            <h2>상품 제목</h2>
+              <p>상품 설명이나 가격 등의 내용</p>
+              <p>가격: ₩100,000</p>
+         </div>
+      </div>
+
+    </div>
+
+    
+    
+    </>
+  );
+};
+
---
+++ .front/src/resources/page/SignUp.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { AddressData } from 'react-daum-postcode';
+import "../css/SignUp.css";
+import DaumPost from './DaumPost';
+const postCodeStyle = {
+    width: '400px'
+};
+
+
+const SignUp = () => {
+    // 회원가입창
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+
+
+    const [isIdCheck, setIsIdCheck] = useState(false);
+    const [isIdAvailable, setIsIdAvailable] = useState(false);
+    
+
+    // DAUM API
+    const [modalState, setModalState] = useState(false);
+    const [inputAddressValue, setInputAddressValue] = useState('');
+    const [inputZipCodeValue, setInputZipCodeValue] = useState('');
+
+    // 다음 주소창 열리게 하기
+    const toggleModal = () => {
+        setModalState(!modalState);
+    };
+
+    const onCompletePost = (data: AddressData) => {    
+        setModalState(false);    
+        setInputAddressValue(data.address);
+        setInputZipCodeValue(data.zonecode);
+    };
+    
+    const handleIdCheck = () => {
+       
+    }
+
+    const handleSignUp = () => {
+        console.log("id" + id);
+        console.log("password" + password);
+        console.log("confirm" + confirm);
+        console.log("address" + inputAddressValue);
+        
+        
+    }
+//========================================================== 
+    return (
+        <>
+            <div className='signupTop'>
+                <div className='signupTop-left'>
+                    <p className='fontfont'>4U에 오신걸 환영합니다!</p>
+                </div>
+                <div className='signupTop-right'>
+                    <img className='signupTopImgOne' src='https://atimg.sonyunara.com/files/attrangs/goods/155318/65aa44fd6a9b2.jpg'/>
+                </div>
+            </div>
+        <div className='formBox'>
+            <div className='formContainer'>
+            <form>
+                <label>
+                    아이디:
+                    <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+                    <button onClick={handleIdCheck}>중복 확인</button>
+                    {isIdCheck && (
+                       <>
+                    {isIdAvailable ? (
+                    <span style={{ color: 'green' }}>사용 가능한 아이디입니다!</span>
+                            ) : (
+                    <span style={{ color: 'red' }}>이미 사용 중인 아이디입니다!</span>
+                            )}
+                        </>
+                    )}
+                </label>
+                <br />
+                <label>
+                    비밀번호:
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    비밀번호 확인:
+                    <input type="password" value={confirm} onChange={(e) => setConfirm(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    생년월일:
+                    <input type="text" value={birthday} onChange={(e) => setBirthday(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    이메일:
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    전화번호:
+                    <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                </label>
+                <br />
+                <label>
+                    주소:
+                    <input type ="text" readOnly value={inputAddressValue} onChange = {(e) => setInputAddressValue(e.target.value)} />
+                <button type="button" onClick={toggleModal}>주소 찾기</button>
+                </label>
+
+                <br />
+            {/* Daum 주소 API 컴포넌트 */}
+            {modalState && <DaumPost onCompletePost={onCompletePost}/>}
+                <label>
+                    상세주소:
+                    <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder='상세 주소를 입력하세요.' />
+                </label>
+                <br />
+                <button type = "button" onClick={handleSignUp}>회원가입</button>
+            </form>
+            </div>
+        </div>
+        </>
+    );
+};
+export default SignUp;
